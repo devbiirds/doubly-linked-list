6bafdb081284f48798076de4123aa8a4093dd89e
vadim <vadikoff57@gmail.com>
2019-10-07 10:42:01 +0300

    feat:add task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,165 @@
 const Node = require('./node');
-
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length =0;
 
-    append(data) {}
+    }
 
-    head() {}
+    append(data) {
+        let node = new Node(data);
+        if(this.length!=0){
+            node.prev = this._tail;
+            this._tail.next = node;
+            this._tail = node;
+            this.length++;
+        }
+        else{
+            this._head = node;
+            this._tail = node;
+            this.length++;
+        }
+        return this;
+    }
 
-    tail() {}
+    head() {
+        if(this._head ==null){
+            return null;
+        }
+        else{
+            return this._head.data;
+        }
+       
+    }
 
-    at(index) {}
+    tail() {
+        if(this._tail == null){
+            return null
+        }
+        else{ return this._tail.data;}
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        let node = this._head;
+        for(let i = 0; i < this.length; i++){
+            if(i == index){
+                return node.data;
+            }
+            else{
+                node = node.next;
+            }
+        }
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        let buf = this._head;
+        let index_buf = 1;
 
-    clear() {}
+        let node = new Node(data);
+        while (buf != null) {
+          
+          buf = buf.next;
+          if (index_buf == index) {
+            buf.prev.next = node;
+            node.prev = buf.prev;
+            node.next = buf;
+            buf.prev = node;
 
-    deleteAt(index) {}
+            this.length++;
+          }
+          index_buf++;
+        }
+        return this;
+      }
+    isEmpty() {
+        if(this.length==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
-    reverse() {}
+    isEmpty() {
+        if(this.length==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
-    indexOf(data) {}
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
+
+    deleteAt(index) {
+        let buf = this._head;
+      let buf_index = 0;
+      while (buf != null) {
+        if (buf_index == index) {
+            
+          if (buf == this._head && buf == this._tail) {
+
+            this._head = null;
+            this._tail = null;
+            this.length = 0;
+            return this;
+          }
+          if (buf == this._head) {
+
+            this._head = this._head.next;
+            this._head.prev = null;
+          } 
+          else if (buf == this._tail) {
+
+            this._tail = this._tail.prev;
+            this._tail.next = null;
+          }
+           else {
+
+            buf.prev.next = buf.next;
+            buf.next.prev = buf.prev;
+          }
+        }
+        buf_index++;
+        buf = buf.next;
+      }
+      this.length--;
+      return this;
+    }
+
+    reverse() {
+       let buf = [];
+       let node = this._head;
+       for(let i = 0; i <this.length ; i++){
+         buf[i] = node.data;
+        node = node.next;
+       }
+       buf.reverse();
+       node = this._head;
+       for(let i = 0; i <this.length ; i++){
+         node.data = buf[i];
+         node = node.next;
+       }
+       return this;
+    }
+
+    indexOf(data) {
+        let node = this._head;
+        for(let i = 0; i < this.length ; i++){
+            if(node.data == data){
+                return i;
+            }
+            else{
+                node = node.next;
+            }
+        }
+        return -1;
+    }
 }
-
 module.exports = LinkedList;
